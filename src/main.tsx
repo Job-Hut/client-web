850bcf76ab74a0c22f095c56f6bcd79748a23578
erica olivia <ecaoliviaaa@gmail.com>
2024-11-03 00:40:19 +0800

    chore: create collection page (-bottom navigation)

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,11 @@ import router from "./config/router";
 import "./index.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "./config/apollo";
-import CollectionList from "./components/ui/CollectionList";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <RouterProvider router={router} />
-      <CollectionList />
     </ApolloProvider>
   </StrictMode>,
 );
