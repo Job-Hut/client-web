260558c7419d0940844ee17a3cd52523624d8638
erica olivia <ecaoliviaaa@gmail.com>
2024-11-03 15:03:22 +0800

    chore: create add collection

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import router from "./config/router";
 import "./index.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "./config/apollo";
+import CreateCollection from "./pages/CreateCollection";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <RouterProvider router={router} />
+      <CreateCollection />
     </ApolloProvider>
   </StrictMode>,
 );
