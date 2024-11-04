c39eaeda846b80c85fe214824d9cb1502cfcff50
Ninzato <ridzakalimanto@gmail.com>
2024-11-04 10:16:40 +0700

    style: edit styling for small screen

--- src/components/ui/ApplicationCard.tsx
+++ src/components/ui/ApplicationCard.tsx
@@ -4,9 +4,9 @@ export default function ApplicationCard() {
   return (
     <Link to="/applications/:_id">
       <div className="flex flex-col justify-between gap-2.5 rounded-lg bg-card p-2 shadow-md">
-        <div className="flex w-full items-center justify-between gap-2.5 rounded-lg bg-application-submitted p-4 md:flex-col md:items-start">
+        <div className="flex w-full flex-col items-start justify-between gap-2.5 rounded-lg bg-application-submitted p-4">
           <span
-            className={`hidden h-fit w-fit rounded-full bg-card px-4 py-2.5 text-sm font-bold text-red-500 md:block`}
+            className={`h-fit w-fit rounded-full bg-card px-2 py-1 text-xs font-medium text-red-500`}
           >
             Due Today
           </span>
@@ -24,20 +24,15 @@ export default function ApplicationCard() {
             </div>
             <div>
               <p className="text-sm">Airbnb</p>
-              <p className="text-lg font-bold">Backend Developer</p>
+              <p className="text-sm font-bold">Backend Developer</p>
             </div>
           </div>
-          <span
-            className={`h-fit w-fit rounded-full bg-card px-4 py-2.5 text-xs font-bold text-red-500 md:hidden`}
-          >
-            Due Today
-          </span>
         </div>
         <div className="px-4">
           <p className="text-sm">Task Todo:</p>
           <div className="flex items-baseline justify-between">
-            <span className="font-semibold">Create CV</span>
-            <span className="text-sm font-semibold">23/10/24</span>
+            <span className="text-sm font-medium">Create CV</span>
+            <span className="text-sm font-medium">23/10/24</span>
           </div>
         </div>
       </div>
--- src/components/ui/Navicon.tsx
+++ src/components/ui/Navicon.tsx
@@ -34,7 +34,7 @@ export default function Navicon({ icon, title, route }: NavIconProps) {
   return (
     <Link
       to={route}
-      className={`flex h-full w-full flex-col items-center gap-1 px-5 py-5 text-sm transition-all duration-100 ease-linear active:bg-primary/5 ${isActive ? "border-t-2 border-primary" : ""}`}
+      className={`mlg:w-full mmd:w-16 flex h-full w-14 flex-col items-center gap-1 px-5 py-5 text-sm transition-all duration-100 ease-linear active:bg-primary/5 ${isActive ? "border-t-2 border-primary" : ""}`}
     >
       <button title={title}>{icon}</button>
     </Link>
