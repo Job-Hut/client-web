d2290baab237c91b3466b15b38ecf31c3be817a5
jalusw <jalusatrianiwijaya1@gmail.com>
2024-11-07 08:55:31 +0800

    fix: cache

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
-import config from "./src/config/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -29,25 +28,6 @@ export default defineConfig({
             src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: config.apiBaseUrl,
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "graphql-cache",
-              networkTimeoutSeconds: 10,
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 10,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
           },
         ],
       },
